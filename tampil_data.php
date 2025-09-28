cb3b93e14db426c53739702e26a7b834458ae1a5
gazarabbani <gazarabbanii.1@gmail.com>
2025-09-28 10:40:44 +0800

    Add files via upload

---
+++ tampil_data.php
@@ -0,0 +1,45 @@
+<?php
+    require_once 'koneksi.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <tittle>Data Pengguna</tittle>
+</head>
+<body>
+
+    <h1>Data Pengguna Terdaftar</h1>
+    <table border = "1" cellpadding = "10" cellspacing = "0">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Tanggal Registrasi</th>
+            </tr>
+        </thead>
+        <?php
+            $query = "SELECT * FROM users";
+            $result = $koneksi->query($query);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . htmlspecialchars($row['nama']) . "</td>";
+                    echo "<td>" . htmlspecialchars($row['email']) . "</td>";
+                    echo "<td>" . $row['tanggal_registrasi'] . "</td>";
+                    echo '<td> <a class="btn btn-warning href="#">update<a/> </td>';
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='4'>Tidak ada data pengguna.</td></tr>";
+            }
+
+            $koneksi->close();
+        ?>
+    </table>
+
+</body>
+</html>
